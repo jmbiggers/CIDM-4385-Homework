7038ef31070a6ed742d80ca7c8485a12ad2eccfb
jmbiggers <julianbiggers@yahoo.com>
2016-02-15 19:40:07 +0000

    HW commit

---
+++ Homework/HW2/HW_2.js
@@ -0,0 +1,12 @@
+var myModule = angular.module('RandomSelector', []);
+myModule.controller('StudentSelector',
+
+    function($scope) {
+
+        myModule.controller('StudentSelector')
+        var ss = this;
+
+
+
+    }
+);
